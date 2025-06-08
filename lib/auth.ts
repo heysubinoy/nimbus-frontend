6d8ab2e8cbac3bdb1215e61563dbe4f63e7d05fe
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2025-06-08 22:19:17 +0530

    add: auto account creation

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,29 +1,49 @@
 // in auth.ts
 
-import { NextAuthOptions, getServerSession } from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-const authOptions: NextAuthOptions = {
-    // Configure one or more authentication providers
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID as string,
-            clientSecret: process.env.GOOGLE_SECRET as string,
-        }),
-        // ...add more providers here
-    ],
+import { NextAuthOptions, getServerSession } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { db } from "@/lib/db";
+import { users } from "@/app/schema/users";
+import { eq } from "drizzle-orm";
 
-    secret: process.env.JWT_SECRET,
-}
+const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    // ...add more providers here
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const existingUser = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, user.email!))
+        .then((rows) => rows[0]);
+      if (!existingUser) {
+        await db.insert(users).values({
+          email: user.email!,
+          name: user.name || "",
+        });
+      }
+
+      return true;
+    },
+  },
+  secret: process.env.JWT_SECRET,
+};
 
 /**
  * Helper function to get the session on the server without having to import the authOptions object every single time
  * @returns The session object or null
  */
-const getSession = () => getServerSession(authOptions)
+const getSession = () => getServerSession(authOptions);
 
-export { authOptions, getSession }
+export { authOptions, getSession };
